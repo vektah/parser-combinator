7dfb51c557f927b5d91b5cf982b197c836753dac
Adam Scarr <adams@benon.com>
2013-09-11 12:58:20 +1000

    Add options and fields as seperate arrays in Message

--- src/main/vektah/parser_combinator/language/proto/Message.php
+++ src/main/vektah/parser_combinator/language/proto/Message.php
@@ -6,10 +6,22 @@ class Message
 {
     public $name;
     public $members;
+    public $fields = [];
+    public $options = [];
 
     public function __construct($name, $members)
     {
         $this->name = $name;
         $this->members = $members;
+
+        foreach ($members as $member) {
+            if ($member instanceof Field) {
+                $this->fields[] = $member;
+            }
+
+            if ($member instanceof Option) {
+                $this->options[] = $member;
+            }
+        }
     }
 }
